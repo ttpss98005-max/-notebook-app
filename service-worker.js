599a28768605fab15649b8df8b5b0c85f4ac8e3e
ttpss98005-max <ttpss98005@gmail.com>
2026-09-26 15:18:56 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(title, options);
 });
 
-const CACHE_NAME = 'jishibu-cache-v73';
+const CACHE_NAME = 'jishibu-cache-v74';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
